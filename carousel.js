8ea8c08a995cae1de966aa2e436154634a050f1f
Kirill Kapralov <Kapralov1501@gmail.com>
2024-10-12 22:33:49 +0300

    fix

--- carousel.js
+++ carousel.js
@@ -1,40 +1,27 @@
-
 (function(d) {
     const trackElement = d.getElementById('track');
     let initX;
     let currentLeft = 0;
     let offset = 0;
-    let bool = true;
 
-    d.getElementById('btn').onclick = () => {
-        bool = false;
-        console.log('bool = false')
-    }
-    trackElement.addEventListener('touchstart', touchStart)
+    trackElement.addEventListener('touchstart', touchStart);
     trackElement.addEventListener('touchmove', touchMove);
     trackElement.addEventListener('touchend', touchEnd);
 
-    function touchStart (event) {
+    function touchStart(event) {
         console.log('start');
         initX = event.touches[0].clientX;
     }
 
-    function touchMove (event) {
-        trackElement.removeEventListener('touchstart', touchStart);
+    function touchMove(event) {
         const currentX = event.touches[0].clientX;
-        offset = currentX - initX
+        offset = currentX - initX;
         trackElement.style.left = currentLeft + offset + 'px'; 
     }
 
-    function touchEnd (event) {
+    function touchEnd() {
         console.log('end');
         currentLeft += offset;
         offset = 0;
-        trackElement.addEventListener('touchstart', touchStart);
     }
-
-
-
-
-
 }(document));
